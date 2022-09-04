d12637c314bdbea257dcc623863a471593c0a231
Zeeshan Ali <mac2015@Macs-MacBook-Pro.local>
2022-09-04 15:54:48 +0500

    createProject function added

--- truffle/contracts/CrowdFunding.sol
+++ truffle/contracts/CrowdFunding.sol
@@ -1,14 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CrowdFunding {
-uint public name;
+contract CrowdFunding{
+  address public owner;
+  uint public balances;
 
-function setName (uint _name)  public{
-  name=_name;
-}  
+struct projectStat{
+uint totalProjects;
+uint totalBacking;
+uint totalCollection;
+}
 
-function getName()  public view returns(uint){
-  return name;
-}  
+struct donarInfo{
+  address owner;
+  uint contribution;
+}
+
+struct projectInfo{
+  uint id;
+  address owner;
+  string title;
+  string description;
+  string imageUrl;
+}
+
+function createProject(
+  string memory title,
+  string memory description,
+  string memory imageUrl
+  //uint cost
+  )public view returns(bool) {
+  require(bytes(title).length > 0,"Title can't be empty");
+  require(bytes(description).length > 0,"Description can't be empty");
+
+  require(bytes(imageUrl).length > 0,"Image can't be empty");
+  projectInfo memory project;
+  project.owner = msg.sender;
+  project.title =title;
+  project.description= description;
+  project.imageUrl= imageUrl; 
+  
+  return true;
+}
 }
