21f3de01105b7eb369340411aeb0f52621345237
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-09-05 18:26:22 +0500

    Initialize React-Router-v6 for SPA

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import Header from './Components/Header';
+import AddProject from './Components/AddProject';
 
 
 function App() {
   return (
    <React.Fragment>
-    <Header />
+      <Routes>
+        <Route path="/" element={<Header  />} />
+        <Route path="/add-project" element={<AddProject />} />
+      </Routes>
    </React.Fragment>
   );
 }
